d37caaf932dcc6645500acdc23a12404736ed029
pranay0124 <pranay0124@msitprogram.net>
2018-08-01 20:35:24 +0530

    compare_AB

---
+++ cspp1-practice/M3/compare_AB.py
@@ -0,0 +1,10 @@
+varA = 10
+varB = 15
+if (type(varA)==str or type(varB)==str):
+	print("String Involved")
+if varA>varB:
+	print("Bigger")
+elif varA<varB:
+	print("Smaller")
+else:
+	print("Equal")
