02a38e7e2f79222a2e40d250e180e121cb20725b
pranay0124 <pranay0124@msitprogram.net>
2018-08-02 20:47:35 +0530

    " -> 5 of 5 passed. pylint: 10.00/10 "

--- cspp1-practice/cspp1-assignments/M4/p3/longest_substring.py
+++ cspp1-practice/cspp1-assignments/M4/p3/longest_substring.py
@@ -8,16 +8,20 @@ def main():
     st_r3 = ''
     ma_x = 0
     co_t = 0
-    for j in range(0,len(st_r1)):
+    j = 0
+    for j in range(0, len(st_r1)-1):
         if ord(st_r1[j]) <= ord(st_r1[j+1]):
             st_r2 = st_r2 + st_r1[j]
             co_t += 1
         else:
+            st_r2 = st_r2 + st_r1[j]
+            co_t += 1
             if ma_x < co_t:
-                st_r3 = st_r2 + st_r1[j]
+                st_r3 = st_r2
                 ma_x = co_t
-                co_t = 0
-            st_r2 = st_r1[j+1]
+            co_t = 0
+            st_r2 = ""
+    st_r2 = st_r2+st_r1[j+1]
     if co_t > ma_x:
         st_r3 = st_r2
     print(st_r3)
