03b9852bfb9689e60a0dfa197662cec620ad50e7
pranay0124 <pranay0124@msitprogram.net>
2018-08-03 18:15:59 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

---
+++ cspp1-practice/M5/p2/square_root.py
@@ -0,0 +1,20 @@
+'''
+Author: Pranay Kumar Yenishetty
+Date: 03-02-2018
+'''
+
+def main():
+    ''' This algorithm is used to calcualte square root '''
+    s_1 = input()
+    ep_i = 0.01
+    gu_ess = 0.0
+    inc_r = 0.1
+    while abs(gu_ess ** 2 - int(s_1)) >= ep_i and gu_ess <= int(s_1):
+        gu_ess += inc_r
+    if abs(gu_ess ** 2 - int(s_1)) >= ep_i:
+        print('Failed on cube root of' + str(s_1))
+    else:
+        print(gu_ess)
+
+if __name__ == "__main__":
+    main()
