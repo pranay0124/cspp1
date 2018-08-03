1c7d9802915d6fb40e9f157ae8b797ea07dbe99c
pranay0124 <pranay0124@msitprogram.net>
2018-08-03 17:03:57 +0530

    " -> 3 of 4 passed. pylint: 10.00/10 "

--- cspp1-practice/M5/p1/perfect_cube.py
+++ cspp1-practice/M5/p1/perfect_cube.py
@@ -4,7 +4,7 @@ Date : 03-08-2018
 '''
 def main():
     ''' This algorithm is to find the given number is a perfect cube or not '''
-    n_1 = int(input("Enter the number"))
+    n_1 = int(input())
     an_s = 0
     while an_s**3 < n_1:
         an_s += 1
@@ -14,4 +14,3 @@ def main():
         print(str(n_1) + ' is not a perfect cube')
 if __name__ == "__main__":
     main()
-    
