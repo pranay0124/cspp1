a767e46035f0aaaec5500d8e84451337b2b6b1d7
pranay0124 <pranay0124@msitprogram.net>
2018-08-04 11:38:08 +0530

    " -> 7 of 7 passed. pylint: 8.24/10 "

--- cspp1-practice/cspp1-assignments/M6/p3/digit_product.py
+++ cspp1-practice/cspp1-assignments/M6/p3/digit_product.py
@@ -7,16 +7,20 @@ def main():
     Program to find the products of all the digits of a given number
     '''
     int_1 = int(input())
-    int_2 = int_1
+    int_2 = abs(int_1)
     int_3 = 0
     mu_l = 1
-    len_1 = len(str(int_1))
+    len_1 = len(str(abs(int_1)))
     while len_1 > 0:
         int_3 = int_2 % 10
         mu_l = mu_l * int_3
         int_2 = int_2 // 10
         len_1 -= 1
-    print(mu_l)
+    if int_1 > 0:
+    	print(mu_l)
+    else:
+    	mu_l = mu_l * -1
+    	print(mu_l)
 
 if __name__ == "__main__":
     main()
