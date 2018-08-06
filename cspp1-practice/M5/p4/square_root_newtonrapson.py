7fe23485238ea36f4be730268421ecccc05e3028
pranay0124 <pranay0124@msitprogram.net>
2018-08-06 13:05:57 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1-practice/M5/p4/square_root_newtonrapson.py
+++ cspp1-practice/M5/p4/square_root_newtonrapson.py
@@ -1,18 +1,18 @@
 '''
 
-Author : Anisha Vulli
-Date : 03 Aug 2018
+Author : Pranay Kumar Y
+Date : 03-04-2018
 
 '''
 
 def main():
     ''' Square root calculation using newton raphson'''
-    nu_m = input()
-    ep_i = 0.01
-    int_num = int(nu_m)
-    gu_ess = int_num/2.0
-    while abs(gu_ess*gu_ess - int_num) >= ep_i:
-        gu_ess = gu_ess - (((gu_ess**2) - int_num)/(2*gu_ess))
+    num_1 = input()
+    epi_1 = 0.01
+    int_1 = int(num_1)
+    gu_ess = int_1/2.0
+    while abs(gu_ess*gu_ess - int_1) >= epi_1:
+        gu_ess = gu_ess - (((gu_ess**2) - int_1)/(2*gu_ess))
     print(str(gu_ess))
 
 if __name__ == "__main__":
