f98230f043ca1e4f4da4e67d8dc148c41306d39c
pranay0124 <pranay0124@msitprogram.net>
2018-08-07 23:00:49 +0530

    " -> 0 of 10 passed. pylint: 10.00/10 "

--- cspp1-practice/M8/p2/assignment2.py
+++ cspp1-practice/M8/p2/assignment2.py
@@ -2,16 +2,15 @@
     date : 07-08-2018'''
 
 def sum_of_digits(n_1):
+    '''function for sum of digits'''
     if n_1 == 1:
-        return n_1
-    else:
-        return (n_1 % 10) + sum_of_digits(n_1//10)
-
+        return 0
+    return (n_1 % 10) + sum_of_digits(n_1//10)
 
 def main():
+    '''main function'''
     a_1 = input()
     print(sum_of_digits(int(a_1)))
 
 if __name__ == "__main__":
     main()
-
