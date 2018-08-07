5cd102eda4b4f75f66f6c5bec01dc35cb6a1bb29
pranay0124 <pranay0124@msitprogram.net>
2018-08-07 22:50:24 +0530

    " -> 0 of 10 passed. pylint: 0/0 "

--- cspp1-practice/M8/p1/assignment1.py
+++ cspp1-practice/M8/p1/assignment1.py
@@ -3,14 +3,16 @@
     '''
 
 def factorial_num(n_1):
-    if n_1 == 1:
+	'''recursive function for factorial'''
+    if n_1 == 1 or n_1 == 0:
         return n_1
     else:
-        return n_1 * factorial_num(n_1 - 1)    
+        return n_1 * factorial_num(n_1 - 1)
 
 def main():
-    a = input()
-    print(factorial_num(int(a)))    
+	'''main function'''
+    a_1 = input()
+    print(factorial_num(int(a_1)))
 
 if __name__ == "__main__":
     main()
