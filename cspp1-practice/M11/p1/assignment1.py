60c0228f6f9e319fa8d553c98eeb889cf2e01f9a
pranay0124 <pranay0124@msitprogram.net>
2018-08-10 20:00:51 +0530

    " -> 9 of 9 passed. pylint: 8.12/10 "

--- cspp1-practice/M11/p1/assignment1.py
+++ cspp1-practice/M11/p1/assignment1.py
@@ -3,7 +3,7 @@ Exercise: Assignment-1
 The first step is to implement some code that allows us to calculate the score for a single word. The function get_word_score should accept as input a string of lowercase letters (a word) and return the integer score for that word, using the game's scoring rules.
 '''
 
-def get_word_score(word, n):
+def get_word_score(word, num):
     """
     Returns the score for a word. Assumes the word is a valid word.
 
@@ -28,10 +28,10 @@ def get_word_score(word, n):
     length_word = len(word)
     for i in word:
         sum += scrabble_letter_values[i]
-    x = sum*length_word
-    if n == length_word:
-        x += 50
-    return x
+    x_1 = sum*length_word
+    if num == length_word:
+        x_1 += 50
+    return x_1
     
 
 
