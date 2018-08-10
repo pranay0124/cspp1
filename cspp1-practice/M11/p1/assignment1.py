0ec200dcfd981d0179e2fe97f267c05167831fa9
pranay0124 <pranay0124@msitprogram.net>
2018-08-10 19:59:15 +0530

    " -> 9 of 9 passed. pylint: 6.25/10 "

--- cspp1-practice/M11/p1/assignment1.py
+++ cspp1-practice/M11/p1/assignment1.py
@@ -19,18 +19,18 @@ def get_word_score(word, n):
     returns: int >= 0
     """
     # TO DO ... <-- Remove this comment when you code this function
-    SCRABBLE_LETTER_VALUES = {
-    'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
-    'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
-    's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
+    scrabble_letter_values = {
+        'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1, 'f': 4, 'g': 2, 'h': 4, 'i': 1,
+        'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
+        's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10}
 
     sum = 0
     length_word = len(word)
     for i in word:
-    	sum += SCRABBLE_LETTER_VALUES[i]
+        sum += scrabble_letter_values[i]
     x = sum*length_word
     if n == length_word:
-    	x +=50
+        x += 50
     return x
     
 
