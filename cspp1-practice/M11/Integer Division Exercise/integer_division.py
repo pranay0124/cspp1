631ccd0c2fa5e426b59deebd5bfbdb5c45ace3a8
pranay0124 <pranay0124@msitprogram.net>
2018-08-10 14:48:53 +0530

    integer division

---
+++ cspp1-practice/M11/Integer Division Exercise/integer_division.py
@@ -0,0 +1,24 @@
+''' Author : Pranay Kumar Y
+    Date : 0-08-2018'''
+
+def integer_division(x, a):
+    """
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the integer division of x divided by a.
+    """
+    count = 0
+    while x >= a:
+        count += 1
+        x = x-a
+    return count    
+
+def main():
+    data = input()
+    data = data.split()
+    print(integer_division(int(data[0]), int(data[1])))
+
+
+if __name__== "__main__":
+    main()
