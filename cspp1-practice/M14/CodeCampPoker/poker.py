523ea3bf4e65e885c58dbc4cea2188bf5979cdfe
pranay0124 <pranay0124@msitprogram.net>
2018-08-14 17:20:14 +0530

    " -> 3 of 3 passed. pylint: 9.74/10 "

--- cspp1-practice/M14/CodeCampPoker/poker.py
+++ cspp1-practice/M14/CodeCampPoker/poker.py
@@ -22,7 +22,7 @@ def is_straight(hand):
     for i in hand:
         face_value.append(i[0])
     adict = {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
-    'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
+             'T':10, 'J':11, 'Q':12, 'K':13, 'A':14}
     for i in adict:
         for j in face_value:
             if i == j:
