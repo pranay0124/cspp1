ee1a6fdbfcc267f912326b066ff6c7633bed0f25
pranay0124 <pranay0124@msitprogram.net>
2018-08-17 10:16:16 +0530

    " -> 0 of 3 passed. pylint: 7.06/10 "

--- cspp1-practice/M16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/M16/CodeCampDocumentDistance/document_distance.py
@@ -7,10 +7,8 @@ def similarity(dict1, dict2):
         Compute the document distance as given in the PDF
     '''
     
-    dict1 = dict1.lower().split()
-    dict2 = dict2.lower().split()
-    dict1 = dict(dict1)
-    dict2 = dict(dict2)
+    dict1 = dict1.lower()
+    dict2 = dict2.lower() 
     dict1 = dict1.strip('!@#$%^&*()?><,./;:')    
     dict2 = dict2.strip('!@#$%^&*()?><,./;:')
     #hand = ['0', '1', '2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*','(',')','?']
