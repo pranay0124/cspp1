7f79b4c96ebab06bfb290100a9bc84a54b4be91c
pranay0124 <pranay0124@msitprogram.net>
2018-08-17 11:07:21 +0530

    " -> 0 of 3 passed. pylint: 5.67/10 "

--- cspp1-practice/M16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/M16/CodeCampDocumentDistance/document_distance.py
@@ -25,8 +25,8 @@ def similarity(dict1, dict2):
     stopword = load_stopwords("stopwords.txt")
     for i in stopword:
         dict1.pop(i, None)
-    # for i in stopword:
-    #     dict2.pop(i, None)
+    for i in stopword:
+        dict2.pop(i, None)
     
     
 
