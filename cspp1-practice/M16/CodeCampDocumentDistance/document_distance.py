74e5914397107de3b12ab14ea4705d753e34fbba
pranay0124 <pranay0124@msitprogram.net>
2018-08-17 10:35:41 +0530

    " -> 0 of 3 passed. pylint: 2.86/10 "

--- cspp1-practice/M16/CodeCampDocumentDistance/document_distance.py
+++ cspp1-practice/M16/CodeCampDocumentDistance/document_distance.py
@@ -14,6 +14,7 @@ def similarity(dict1, dict2):
     #hand = ['0', '1', '2','3','4','5','6','7','8','9','!','@','#','$','%','^','&','*','(',')','?']
     dict1 = dict1.replace("'","")
     dict2 = dict2.replace("'","")
+    print(stopwords)
 
 def load_stopwords(filename):
     '''
