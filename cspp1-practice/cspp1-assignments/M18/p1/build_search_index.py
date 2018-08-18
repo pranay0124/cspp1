1ef59d609ae46c76a09b4cf4e700ac83560a9a1a
pranay0124 <pranay0124@msitprogram.net>
2018-08-18 11:04:31 +0530

    " -> 0 of 1 passed. pylint: 9.27/10 "

--- cspp1-practice/cspp1-assignments/M18/p1/build_search_index.py
+++ cspp1-practice/cspp1-assignments/M18/p1/build_search_index.py
@@ -73,9 +73,10 @@ def build_search_index(docs):
 
     # return search index
     a_1 = []
+    adict = {}
     for i in docs:
-        a_1 = word_list(i)
-        adict = dict(collections.Counter(a_1))
+        a_1 = a_1 + word_list(i)
+        adict = adict + dict(collections.Counter(a_1))
         
     print(adict)
     return adict
