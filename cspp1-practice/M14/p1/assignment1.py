46df73618f8a3594dbdb72c595c96a6d8153db0d
pranay0124 <pranay0124@msitprogram.net>
2018-08-21 22:57:24 +0530

    " -> 0 of 5 passed. pylint: 0/0 "

--- cspp1-practice/M14/p1/assignment1.py
+++ cspp1-practice/M14/p1/assignment1.py
@@ -8,7 +8,8 @@ class Cipher:
         small_alphabet = "-" + string.ascii_lowercase + string.ascii_lowercase
         upper_alphabet = "-" + string.ascii_uppercase + string.ascii_uppercase
         shifted_string = ""
-        for i in range(0, len(self.value)):
+        a = len(self.value)
+        for i in range a:
             if self.value[i] in small_alphabet:
                 shifted_string += small_alphabet[small_alphabet.index(self.value[i]) + shift_number]
             elif self.value[i] in upper_alphabet:
