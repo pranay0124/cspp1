1597e0a61e3810722eb507bc42dc86867eb05bee
pranay0124 <pranay0124@msitprogram.net>
2018-08-21 22:37:27 +0530

    " -> 0 of 5 passed. pylint: -10.00/10 "

--- cspp1-practice/M14/p1/assignment1.py
+++ cspp1-practice/M14/p1/assignment1.py
@@ -6,12 +6,12 @@ class Cipher:
         upper_alphabet = ""
         small_alphabet = "-" + string.ascii_lowercase + string.ascii_lowercase
         upper_alphabet = "-" + string.ascii_uppercase + string.ascii_uppercase
-        shifted_string = " "
+        shifted_string = ""
         for i in range (0, len(self.text)):
             if self.text[i] in small_alphabet:
-                shifted_string += small_alphabet[small_alphabet[index(self.text(i))] + shift_number]
+                shifted_string += small_alphabet[small_alphabet.index(self.text[i]) + shift_number]
             elif self.text[i] in large_alphabet:
-                shifted_string += large_alphabet[large_alphabet[index(self.text(i))] + shift_number]
+                shifted_string += large_alphabet[large_alphabet.index(self.text[i]) + shift_number]
             else:
                 shifted_string += self.text[i]
 
@@ -26,7 +26,6 @@ def main():
     shift_number = int(input())
     Cipher_obj = Cipher(data_input)
     Cipher_obj.shift(shift_number)
-    
     print(data.apply_shift(int(input())))
 
 if __name__ == "__main__":
