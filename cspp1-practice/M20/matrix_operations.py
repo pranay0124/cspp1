d816e88a2182692e17073e44ac0db5726e91db9b
pranay0124 <pranay0124@msitprogram.net>
2018-08-23 12:23:46 +0530

    " -> 4 of 8 passed. pylint: 4.32/10 "

--- cspp1-practice/M20/matrix_operations.py
+++ cspp1-practice/M20/matrix_operations.py
@@ -1,4 +1,4 @@
-def mult_matrix(m1, m2):
+def mult_matrix(m_1, m_2, r_1, r_2, c_1, c_2):
     '''
         check if the matrix1 columns = matrix2 rows
         mult the matrices and return the result matrix
@@ -6,9 +6,21 @@ def mult_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for mult"
     '''
-    pass
+    multiplication_matrix = []
+    if c_1 == r_2:
+        for i in range (r_1):
+            temp = []
+            for j in range(r_1):
+                sum_val = 0
+                for k in range(c_1):
+                    sum_val = sum_val + (m_1[i][k] * m_2[k][j])
+                temp.append(sum_val)
+            multiplication_matrix.append(temp)
+        return multiplication_matrix
+    else:
+        return "Error: Matrix shapes invalid for mult"
 
-def add_matrix(m1, m2):
+def add_matrix(m_1, m_2, r_1, r_2, c_1, c_2):
     '''
         check if the matrix shapes are similar
         add the matrices and return the result matrix
@@ -16,7 +28,16 @@ def add_matrix(m1, m2):
         and return None
         error message should be "Error: Matrix shapes invalid for addition"
     '''
-    pass
+    addition_matrix = []
+    if r_1 == r_2 and c_1 == c_2:
+        for i in range (c_1):
+            temp = []
+            for j in range (c_1):
+                temp.append(m_1[i][j] + m_2[i][j])
+            addition_matrix.append(temp)
+        return addition_matrix
+    else:
+        return "Error: Matrix shapes invalid for addition"
 
 def read_matrix():
     '''
@@ -26,30 +47,30 @@ def read_matrix():
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
     '''
-    a = input().split(",")
-    m = int(a[0])
-    n = int(a[1])
+    dimensions = input().split(",")
+    m = int(dimensions[0])
+    n = int(dimensions[1])
     mat = []
     for i in range(n):
-    	mat.append([])
+        mat.append([])
     for i in range (m):
-    	for j in range (n):
-    		mat[i].append(int(input()))
-    # for i in range (m):
-    # 	for j in range (n):
-    # 	    mat[i][j] = int(input().split())
-    print(mat)
+        temp = input().split()
+        for j in range (n):
+            mat[i].append(int(temp[j]))
+    return mat, m, n
 
 
 def main():
-    matrix_1 = read_matrix()
+    (matrix_1, row_1, column_1) = read_matrix()
 
-    # read matrix 2
+    (matrix_2, row_2, column_2) = read_matrix()
 
-    # add matrix 1 and matrix 2
+    addition_matrix = add_matrix(matrix_1,matrix_2,row_1,row_2,column_1,column_2)
 
-    # multiply matrix 1 and matrix 2
-    pass
+    multiplication_matrix = mult_matrix(matrix_1,matrix_2,row_1,row_2,column_1,column_2)
+    
+    print(addition_matrix)
+    print(multiplication_matrix)
 
 if __name__ == '__main__':
     main()
