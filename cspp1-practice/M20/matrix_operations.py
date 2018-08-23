1c005a3eb4ab0659a0ef4da8ce649217670eac69
pranay0124 <pranay0124@msitprogram.net>
2018-08-23 14:09:16 +0530

    " -> 4 of 8 passed. pylint: 3.96/10 "

--- cspp1-practice/M20/matrix_operations.py
+++ cspp1-practice/M20/matrix_operations.py
@@ -77,9 +77,11 @@ def main():
 
     (matrix_2, row_2, column_2,flag_2) = read_matrix()
 
+    addition_matrix = add_matrix(matrix_1,matrix_2,row_1,row_2,column_1,column_2)
+    multiplication_matrix = mult_matrix(matrix_1,matrix_2,row_1,row_2,column_1,column_2)
+
     if flag_1 == True and flag_2 == True:
-        addition_matrix = add_matrix(matrix_1,matrix_2,row_1,row_2,column_1,column_2)
-        multiplication_matrix = mult_matrix(matrix_1,matrix_2,row_1,row_2,column_1,column_2)
+
         print(addition_matrix)
         print(multiplication_matrix)
 
