d7a7e48fd1a82978f34df3238a66a666c760219b
pranay0124 <pranay0124@msitprogram.net>
2018-08-24 19:35:57 +0530

    " -> 2 of 2 passed. pylint: 9.03/10 "

--- cspp1-practice/cspp1-assignments/M12/p2/social_network.py
+++ cspp1-practice/cspp1-assignments/M12/p2/social_network.py
@@ -14,6 +14,8 @@ def follow(network, arg1, arg2):
     '''
     if arg1 in network:
         network[arg1].append(arg2)
+    else:
+        network[arg1] = [arg2]
     return network
 
 def unfollow(network, arg1, arg2):
@@ -49,7 +51,6 @@ def delete_person(network, arg1):
     for person in network:
         if arg1 in network[person]:
             network[person].remove(arg1)
-
     # for i in network:
     #     for k in [network[0]][k] == str(arg1):
     #         print(network)
