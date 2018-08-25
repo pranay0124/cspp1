528ad27c77618401a50ce001c985264d5e12c66a
pranay0124 <pranay0124@msitprogram.net>
2018-08-25 12:49:40 +0530

    " -> 0 of 26 passed. pylint: 7.10/10 "

--- cspp1-practice/cspp1-assignments/M22/Check Sudoku/check_sudoku.py
+++ cspp1-practice/cspp1-assignments/M22/Check Sudoku/check_sudoku.py
@@ -22,7 +22,6 @@ def is_vertical(sudoku):
     for i in range(len(sudoku)):
         for j in range(i):
             set_v.add(sudoku[j][i])
-            break
             print(set_v)
         # set_v.add(i[0])
     if len(set_v) == 9:
