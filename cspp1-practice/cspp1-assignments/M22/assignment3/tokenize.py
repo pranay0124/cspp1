613ef4a545b4808bf77b0c8187769c2eb7a921f6
pranay0124 <pranay0124@msitprogram.net>
2018-08-25 11:04:57 +0530

    " -> 0 of 4 passed. pylint: 5.00/10 "

--- cspp1-practice/cspp1-assignments/M22/assignment3/tokenize.py
+++ cspp1-practice/cspp1-assignments/M22/assignment3/tokenize.py
@@ -2,7 +2,7 @@
 Write a function to tokenize a given string and return a dictionary with the frequency of
 each word
 '''
-
+import collections
 def tokenize(string):
     string = list(collections.Counter(string))
     return string
